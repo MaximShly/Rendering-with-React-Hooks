123ca65f5b29775681bb4fc5c93434513167810d
MaximShly <118232796+MaximShly@users.noreply.github.com>
2023-07-26 20:27:06 -0400

    Add files via upload

---
+++ reactIntro.jsx
@@ -0,0 +1,44 @@
+const App = () => {
+  const { useState } = React;
+
+  let [state, setState] = useState({
+    initialized: false,
+    productName: null,
+    productDescription: null,
+    productPrice: null,
+  });
+
+  const initializeState = () => {
+    console.log('initializing state...');
+    // Here we set the state object to have values according to the instructions
+    setState({
+      initialized: true,
+      productName: "Rice Krispies",
+      productDescription: "a cereal made of popped rice",
+      productPrice: "$3.00",
+    });
+
+    return state;
+  };
+  
+  // Log to ensure you have the correct values in your state
+  console.log('State: ', state);
+  
+  const { initialized, productName, productDescription, productPrice } = state;
+
+  // Use string interpolation to construct the sentence
+  const stringToReturn = `The product name is ${productName}, product description is ${productDescription}, and product price is ${productPrice}.`;
+
+  if (initialized) {
+    return stringToReturn;
+  }
+
+  return React.createElement(
+    'button',
+    { id: 'initial-button', onClick: () => initializeState() },
+    'Click here to initialize your state'
+  );
+};
+
+//---------------
+ReactDOM.render(<App />, document.getElementById('jsx-container'));
